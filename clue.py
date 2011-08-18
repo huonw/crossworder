14df5535445d4671d38c13c94ee773982bc3808c
Huon Wilson <wilson.huon@gmail.com>
2011-08-19 01:26:25 +1000

    Labelled clues (for references), and better LaTeX output

--- clue.py
+++ clue.py
@@ -1,18 +1,31 @@
-import re
-RE_CLUE_NUMBER = re.compile(r'^\([0-9]+([,-.][0-9]+)*\)$')
+import re, sys
+RE_SPLIT_NAME_BEGIN = re.compile(r'^\<([^>]*)\>')
+RE_SPLIT_NAME = re.compile(r'\<([^>]*)\>')
+RE_CLUE_NUMBER = re.compile(r'^\([0-9]+([,-.](\([^\)]*\))?[0-9]+)*\)$')
 RE_PUNCT = re.compile(r'[,.-]')
 RE_WORD_SPLIT = re.compile(r'([ ,.-])')
 
-def tokenise_line(s):
-    # recieve line in the form "direction|x|y|answer|clue"
+def tokenise_line(_s):
+    # recieve line in the form "[<name>]direction|x|y|answer|clue"
+    # if an answer spans multiple clues, then <name> can be placed just before
+    # the clue break.
+    # e.g. "exit strategy" over two clues is "exit <otherclue>strategy", or "(4,<otherclue>8)"
+    # in clues, <name> will expand to "3 across" or whatever the clue <name> is
+    name = None
+    s = _s.strip()
     try:
+        m = RE_SPLIT_NAME_BEGIN.match(s)
+        if m:
+            name = m.group(1)
+            s = s.split('>',1)[1]
+            
         t = tuple(s.split('|',4))
         (_direction,_x,_y,_answer,clue) = t
     except ValueError as e:
         raise ValueError("Line could not be parsed: %s" % s)
     data = {}
     
-    first_direction = _direction[:1].lower()
+    first_direction = _direction.strip()[:1].lower()
     if first_direction == 'a':
         direction = Direction.ACROSS 
     elif first_direction == 'd':
@@ -30,7 +43,7 @@ def tokenise_line(s):
         answer = None
         lengthstring = _answer.strip('()')
     else:
-        answer = _answer
+        answer = RE_SPLIT_NAME.sub('',_answer.strip())
         
         lengthbuilder = []
         for (i,w) in enumerate(RE_WORD_SPLIT.split(answer)):
@@ -45,12 +58,15 @@ def tokenise_line(s):
         lengthstring = ''.join(lengthbuilder) 
     length = sum([int(x) for x in RE_PUNCT.split(lengthstring)])
     
-    return (direction,x,y,answer,lengthstring,length,clue)
+    return (name,direction,x,y,answer,lengthstring,length,clue)
 
-def dir2str(dirr,long=False):
+def dir2str(dirr,long=False,capital=False):
     names = ['a','d']
     if long:
         names = ['across','down']
+    
+    if capital:
+        names = [x.title() for x in names]
         
     if dirr == Direction.ACROSS:
         return names[0]
@@ -62,11 +78,12 @@ class Direction:
     DOWN = 2
 
 class Clue(object):
-    def __init__(self,direction,x=None,y=None,answer=None,lenstring=None,length=None,clue=None):
+    def __init__(self,direction,name=None,x=None,y=None,answer=None,lenstring=None,length=None,clue=None):
     
         # if there is one argument, try to parse it as a line
-        if len(set([x,y,answer,lenstring,length,clue])) == 1 and x == None:
-            (direction,x,y,answer,lenstring,length,clue) = tokenise_line(direction)
+        if len(set([name,x,y,answer,lenstring,length,clue])) == 1 and x == None:
+            (name,direction,x,y,answer,lenstring,length,clue) = tokenise_line(direction)
+        self._name = name
         self._direction = direction
         self._x = x
         self._y = y
@@ -75,14 +92,23 @@ class Clue(object):
         self._length = length
         self._clue = clue
     
+    def name(self,name=None):
+        if name:
+            self._name = name
+        else:
+            return self._name
+    
     def number(self,n=None):
-        if n != None:
+        if n:
             self._number = n
         else:
             return self._number
     
-    def clue(self):
-        return self._clue
+    def clue(self,clue=None):
+        if clue:
+            self._clue = clue
+        else:
+            return self._clue
     def lengthstring(self):
         return self._lengthstring
     
@@ -97,6 +123,23 @@ class Clue(object):
     
     def is_across(self):
         return self._direction == Direction.ACROSS
+         
+    def direction_name(self,long=False,capital=False):
+        return dir2str(self._direction,long,capital)
+         
+    def resolve_names(self,clues):
+        newclue = []
+        for i,s in enumerate(RE_SPLIT_NAME.split(self._clue)):
+            print(i,s,file=sys.stderr)
+            if i % 2:
+                if s in clues:
+                    c = clues[s]
+                    newclue.append('%d %s' % (c.number(),c.direction_name(True)))
+                else:
+                    raise ValueError("Named clue '%s' not found" % s)
+            else:
+                newclue.append(s)
+        self._clue = ''.join(newclue)
          
     def __str__(self):
         return '%i. %s at (%i,%i) "%s" => "%s" (%s)' % (
--- crossworder.py
+++ crossworder.py
@@ -1,13 +1,31 @@
-import re, clue as c
+import re, clue,sys
         
 
 def load_clues(iterable):
-    clues = []
-    for clue in iterable:
-        stripped = clue.strip()
-        if stripped and stripped[0] != '#':
-            clues.append(c.Clue(stripped))
-    return clues
+    clues = {}
+    metadata = {}
+    count = 0
+    for cl in iterable:
+        stripped = cl.strip()
+        if stripped:
+            start = stripped[0]
+            if start == '@':
+                # metadata
+                try:
+                    title,data = stripped[1:].split(':',1)
+                    metadata[title.lower().strip()] = data.strip()
+                except:
+                    pass
+            elif start != '#':
+                c = clue.Clue(stripped)
+                id =  c.name()
+                if not id:
+                    id = count 
+                    count += 1
+                    
+                clues[id] = c
+    print(clues,file=sys.stderr)            
+    return (metadata,clues)
     
 def from_file(filename):
     return load_clues(open(filename,'rU'))
@@ -17,23 +35,23 @@ def make_grid(clues):
     minx,maxx = (float('inf'),float('-inf'))
     miny,maxy = minx,maxx
     
-    for c in clues:
-        x,y = c.endpoint()
-        if x > maxx:
-            maxx = x
-        if x < minx:
-            minx = x
-        if y > maxy:
-            maxy = y
-        if y < miny:
-            miny = y
+    for name,c in clues.items():
+        for x,y in (c.endpoint(),c.startpoint()):
+            if x > maxx:
+                maxx = x
+            if x < minx:
+                minx = x    
+            if y > maxy:
+                maxy = y
+            if y < miny:
+                miny = y
 
     xlen = maxx - minx
-    ylen = maxy - miny 
-    
+    ylen = maxy - miny
+    print(xlen,ylen,file=sys.stderr)
     grid = [[None] * xlen for _ in range(ylen)]
     
-    for c in clues:
+    for name,c in clues.items():
         x,y = c.startpoint()
         endx,endy = c.endpoint()
         
@@ -64,10 +82,12 @@ def make_grid(clues):
                         c[0].number(count)
                     if c[1]:
                         c[1].number(count)
-
+    for name,c in clues.items():
+        c.resolve_names(clues)
+        
     return grid
     
-def render_as_latex(grid):
+def render_as_latex(grid,metadata={}):
     ylen = len(grid)
     xlen = len(grid[0])
     
@@ -78,13 +98,22 @@ def render_as_latex(grid):
 
 \setlength\parindent{0pt}
 
-\begin{document}
-\thispagestyle{empty}
-\begin{figure}[h]
+\begin{document}''']
+    
+    
+    if 'title' in metadata and 'author' in metadata:
+        latex.append(r'\title{%s}'%metadata['title'])
+        latex.append(r'\author{%s}'%metadata['author'])
+        latex.append(r'\date{}\maketitle')
+    
+    latex.append(r'''\thispagestyle{empty}\begin{figure}[h]
     \centering
-        \begin{tikzpicture}[scale=0.7,every node/.style={font=\scriptsize}]''',r'\draw[black] (0,%d) grid (%d,0);' % (-ylen,xlen)]
+        \begin{tikzpicture}[scale=0.8,number/.style={below right,font=\scriptsize}]''')
+    latex.append(r'\draw[black] (0,%d) grid (%d,0);' % (-ylen,xlen))
+    
     across = []
     down = []
+    
     for i,row in enumerate(grid):
         for j,c in enumerate(row):
             if c:
@@ -96,7 +125,7 @@ def render_as_latex(grid):
                         num = c[1].number()
                         down.append(c[1])
 
-                    latex.append(r'\node[below right] at (%d,%d) {%d};' % (j,-i,num))
+                    latex.append(r'\node[number] at (%d,%d) {%d};' % (j,-i,num))
             else:
                 latex.append(r'\fill[black] (%d,%d) rectangle (%d,%d);' % (j,-i,j+1,-i-1))
     latex.append(r'\end{tikzpicture}\end{figure}')
@@ -117,8 +146,9 @@ if __name__ == '__main__':
     f = sys.stdin
     if len(sys.argv) > 1:
         f = open(sys.argv[1],'rU')
-    g = make_grid(load_clues(f))
-    print(render_as_latex(g))
+    m,cl=load_clues(f)
+    g = make_grid(cl)
+    print(render_as_latex(g,m))
     
     
     
